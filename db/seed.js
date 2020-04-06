ee4df79c351580252e76bd2e1169fd3dd9fd1e5c
Dennis Rivera <rivd19@outlook.com>
2020-04-06 14:37:05 -0400

    set up movie data and mapped through the data inside the seed file

--- db/seed.js
+++ db/seed.js
@@ -26,3 +26,28 @@ TV.remove({}).then(() => {
       console.log(err);
     });
 });
+
+const moviesData = data.results.map((item) => {
+  const trendingMovies = {
+    name: item.title,
+    description: item.overview,
+    rating: item.vote_average,
+    releaseDate: item.release_date,
+    image: item.poster_path,
+    media: item.media_type,
+  };
+
+  return trendingMovies;
+});
+console.log(moviesData);
+
+Movies.remove({}).then(() => {
+  Movies.create(moviesData)
+    .then((showMovies) => {
+      console.log(showMovies);
+      process.exit();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
