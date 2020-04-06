2c07d2a550cacb55318f84d2e510f64a052238f1
Dennis Rivera <rivd19@outlook.com>
2020-04-06 14:34:04 -0400

    Added tv schema

---
+++ models/Tv.js
@@ -0,0 +1,13 @@
+const mongoose = require("../db/connection");
+const Schema = mongoose.Schema;
+
+const TV = new Schema({
+  title: String,
+  description: String,
+  rating: Number,
+  releaseDate: String,
+  image: String,
+  media: String,
+});
+
+module.exports = mongoose.model("TV", TV);
