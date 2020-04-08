dd503cc7c7e2fe835eaf000885de71cc53b30ff5
Dennis Rivera <rivd19@outlook.com>
2020-04-08 17:35:56 -0400

    Updated image urls

--- db/fetchTv.js
+++ db/fetchTv.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const fetch = require("node-fetch");
 const url =
-  "https://api.themoviedb.org/3/trending/tv/week?api_key=614c211b2d4c03410bfb2e7b28367475&page=2";
+  "https://api.themoviedb.org/3/trending/tv/week?api_key=614c211b2d4c03410bfb2e7b28367475&page=3";
 
 fetch(url)
   .then((res) => res.json())
   .then((res) => {
     console.log(res);
     let tv = JSON.stringify(res);
-    fs.writeFile("./db/tvData2.json", tv, (err) => {
+    fs.writeFile("./db/tvData3.json", tv, (err) => {
       if (err) {
         console.log(err);
       } else {
